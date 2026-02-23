8b95084ebf5545be491ca07d700f13237facc4e4
NMProgram <1124834@hr.nl>
2026-02-23 14:00:11 +0100

    Created basic layout of classes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿public static class Program
+{
+    static void Main(string[] args)
+    {
+        Monster monster = new();
+        Console.WriteLine(monster);
+        Location location = new();
+        Console.WriteLine(location);
+        Player player = new();
+        Console.WriteLine(player);
+        Quest quest = new();
+        Console.WriteLine(quest);
+    }
+}
