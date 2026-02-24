b0929150b94bf9faefc7f23832c61ac355cec159
DDenial0 <221677108+DDenial0@users.noreply.github.com>
2026-02-24 14:06:07 +0100

    Improve Monster.cs Remove tests Program.cs

--- Program.cs
+++ Program.cs
@@ -6,22 +6,5 @@
         string PlayerName = Console.ReadLine();
         Player player = new(PlayerName, 50, 50, World.Weapons[0], World.Locations[0]);
         player.PrintStats();
-
-        // ==============================================================================
-        // Monster testing
-        // ==============================================================================
-        Monster test_subject = new(20, "Test Subject", 10.0, 100.0, 100.0);
-        // test 1: taking damage & health stats - SUCCESS
-        // test 2: taking damage with random defense between 0 and 40% - SUCCESS
-        
-        test_subject.TakeDamage(10, true);
-        test_subject.TakeDamage(10, true);
-        test_subject.TakeDamage(10, false);
-        test_subject.TakeDamage(10, true);
-        test_subject.TakeDamage(10, true);
-        test_subject.TakeDamage(10, false);
-        test_subject.TakeDamage(10, false);
-        // ==============================================================================
-
     }
 }
