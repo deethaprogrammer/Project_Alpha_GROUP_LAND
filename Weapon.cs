aa60565a2f557be9bf871fa80a6f7bfcf298779c
Deethaprogrammer <vdheidendamian@gmail.com>
2026-02-27 16:13:44 +0100

    only bow methods and object not yet implemented

--- Weapon.cs
+++ Weapon.cs
@@ -32,38 +32,3 @@ public class Weapon
     }
 }
 
-// public static class WeaponSystem
-// {
-//     public static Weapon CurrentWeapon = new Weapon("None", 0);
-
-//     public static void ChangeWeapon()
-//     {
-//         Console.WriteLine("CHOOSE YOUR WEAPON");
-//         Console.WriteLine("A. SWORD (Damage 40)");
-//         Console.WriteLine("B. BOW (Damage 25, Arrows 5)");
-//         Console.WriteLine("C. SPEAR (Damage 30)");
-
-//         string choice = Console.ReadLine() ?? "";
-
-//         switch (choice.ToUpper())
-//         {
-//             case "A":
-//                 CurrentWeapon = new Weapon(4, "Sword", 40);
-//                 break;
-
-//             case "B":
-//                 CurrentWeapon = new Weapon(5, "Bow", 25);
-//                 break;
-
-//             case "C":
-//                 CurrentWeapon = new Weapon(6, "Spear", 30);
-//                 break;
-
-//             default:
-//                 Console.WriteLine("INVALID CHOICE");
-//                 return;
-//         }
-
-//         Console.WriteLine(CurrentWeapon.Name + " EQUIPPED");
-//     }
-// }
